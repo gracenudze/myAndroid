4f8f754e6e35ae8a5e46039fa8a35f574359c2ce
Grace Nudze <graceynud@gmail.com>
2021-11-10 16:49:15 +0000

    First Assignment

---
+++ FirstIssue/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick (View view) {
+        EditText editUsername = findViewById(R.id.editTextUsername);
+        EditText editEmail = findViewById(R.id.editTextEmail);
+        EditText editPassword = findViewById(R.id.editTextPassword);
+        Button registerButton = findViewById(R.id.buttonRegister);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView viewUsername = findViewById(R.id.editTextUsername);
+                TextView viewEmail = findViewById(R.id.editTextEmail);
+                TextView viewPassword = findViewById(R.id.editTextPassword);
+
+                viewUsername.setText(editUsername.getText().toString());
+                viewEmail.setText(editEmail.getText().toString());
+                viewPassword.setText( editPassword.getText().toString());
+
+                Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
