fd28d0af919b3a4df366765d08274c9457ffc32d
gracenudze <64440719+gracenudze@users.noreply.github.com>
2021-11-19 10:52:24 +0000

    Feature/register screen UI (#7)
    * Android UI Desgin
    
    * Changes to UI design
    
    * Changes made
    
    * Changes made to the margin

---
+++ app/src/main/java/com/example/firstapplication/RegisterUserActivity.java
@@ -0,0 +1,48 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+
+    public void onBtnClick (View view) {
+        EditText etFirstName = findViewById(R.id.etFirstName);
+        EditText etLastName = findViewById(R.id.etLastName);
+        EditText etPassword = findViewById(R.id.etPassword);
+        EditText etPhoneNumber = findViewById(R.id.etPhone);
+        Button registerButton = findViewById(R.id.btnRegister);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tvFirstName = findViewById(R.id.etFirstName);
+                TextView tvLastName = findViewById(R.id.etLastName);
+                TextView tvPassword = findViewById(R.id.etPassword);
+                TextView tvPhoneNumber = findViewById(R.id.etPhone);
+
+                tvFirstName.setText(etFirstName.getText().toString());
+                tvLastName.setText(etLastName.getText().toString());
+                tvPassword.setText(etPassword.getText().toString());
+                tvPhoneNumber.setText(etPhoneNumber.getText().toString());
+
+                Intent intent = new Intent(RegisterUserActivity.this, HomePageActivity.class);
+                intent.putExtra(ConstantUtils.username, etFirstName.getText().toString());
+                intent.putExtra(ConstantUtils.email, etLastName.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+}
