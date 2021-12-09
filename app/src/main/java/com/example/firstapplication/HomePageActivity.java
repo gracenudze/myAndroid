ec395710120373038ba94eacd84c798bc320484e
Grace Nudze <graceynud@gmail.com>
2021-12-09 16:04:14 +0000

    Navigation and RecyclerView

--- app/src/main/java/com/example/firstapplication/HomePageActivity.java
+++ app/src/main/java/com/example/firstapplication/HomePageActivity.java
@@ -1,10 +1,17 @@
 package com.example.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class HomePageActivity extends AppCompatActivity {
+
+    ArrayList<ButtonNames> buttonNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +19,20 @@ public class HomePageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         TextView tvUsername = findViewById(R.id.tvUsername);
-        tvUsername.setText(getIntent().getExtras().getString(ConstantUtils.firstName) + " " +
-                getIntent().getExtras().getString(ConstantUtils.lastName));
+        tvUsername.setText(new StringBuilder().append(getIntent().getExtras().getString(ConstantUtils.firstName))
+                .append(" ").append(getIntent().getExtras().getString(ConstantUtils.lastName)).toString());
 
+        // Lookup the recyclerview in activity layout
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // Initialize contacts
+        buttonNames = ButtonNames.createButtonNamesList(4);
+        // Create adapter passing in the sample user data
+         RecyclerViewAdapter adapter = new RecyclerViewAdapter(buttonNames);
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.setAdapter(adapter);
+        // Set layout manager to position the items
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
     }
-
 }
